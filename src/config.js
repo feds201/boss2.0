610a4d42634d13e732318d4baf9ce7cc02a597b6
Ritesh Raj Arul Selvan <riteshrajas.arulselvan@gmail.com>
2025-01-03 00:03:54 -0500

    Revert "Add .env to .gitignore to prevent sensitive information from being tracked"
    This reverts commit eb7610338ad7e894ed7f7da4dfb5c279eed5845e.

--- src/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    DISCORD_TOKEN: process.env.DISCORD_TOKEN,
-    CHANNEL_ID: process.env.CHANNEL_ID,
-    GUILD_ID: process.env.GUILD_ID,
-    CLIENT_ID: process.env.CLIENT_ID,
-    PORT: process.env.PORT || 3000
-};
